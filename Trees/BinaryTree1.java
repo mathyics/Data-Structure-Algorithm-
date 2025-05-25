f6973ff8de309e7c770ec985ce7444fd75a1d9e9
[yashwanth v] <[vyashwanth179@gmail.com]>
2025-05-25 18:13:55 +0530

    Level order tree traversal

--- Trees/BinaryTree1.java
+++ Trees/BinaryTree1.java
@@ -1,4 +1,5 @@
 package Trees;
+import java.util.*;
 public class BinaryTree1 {
     static class Node {
         int data;
@@ -48,6 +49,37 @@ public class BinaryTree1 {
             postorder(root.right);
             System.out.print(root.data + " ");
         }
+
+        //level order traversal
+        public static void levelOrder(Node root) {
+            if(root==null){
+                return;
+            }
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while(!q.isEmpty()){
+                Node currNode=q.remove();
+                if(currNode==null){
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    }else{
+                        q.add(null);
+                    }
+                }else{
+                    System.out.print(currNode.data+" ");
+                    if(currNode.left!=null){
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right!=null){
+                        q.add(currNode.right);
+                    }
+                }
+            }
+
+        }
     }
 
     public static void main(String args[]) {
@@ -61,6 +93,9 @@ public class BinaryTree1 {
         tree.inorder(root);
         System.out.println("\nPostorder Traversal: ");
         tree.postorder(root);
+
+        System.out.println("\nLevel Order Traversal: ");
+        tree.levelOrder(root);
 
 
     }
