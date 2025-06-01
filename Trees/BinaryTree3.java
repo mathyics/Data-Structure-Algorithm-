ff46455a842f2781f5aa2f36d54da25e262074bb
[yashwanth v] <[vyashwanth179@gmail.com]>
2025-06-01 12:21:20 +0530

    Lowest common Ancestor

--- Trees/BinaryTree3.java
+++ Trees/BinaryTree3.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class BinaryTree3{
     public static class Node {
         int data;
@@ -20,6 +22,40 @@ class BinaryTree3{
         kLevel(root.right, level+1, k);
 
     }
+    // Lowest Common Ancestor (LCA) of a Binary Tree
+    public static boolean getPath(Node root , int n, ArrayList<Node> path){
+        if(root==null){
+            return false;
+        }
+        path.add(root);
+        if(root.data==n){
+            return true;
+        }
+        boolean FoundLeft=getPath(root.left,n, path);
+        boolean FoundRight=getPath(root.right,n, path);
+        if( FoundLeft || FoundRight)
+        {
+            return true;
+        }
+
+        path.remove(path.size()-1); // backtrack if not found
+        return false;
+
+    }
+    public static Node lca(Node root, int n1, int n2) {
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+        getPath(root,n1,path1);
+        getPath(root,n2,path2);
+         int i=0;
+        for(;i<path1.size()&&i<path2.size();i++)
+            if(path1.get(i)!=path2.get(i)){
+                break;
+            }
+        
+        Node lca = path1.get(i-1);
+        return lca;
+    }
 
     public static void main(String[] args) {
         Node root = new Node(1);
@@ -30,9 +66,16 @@ class BinaryTree3{
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-      int k =2;
-        System.out.println("Nodes at level " + k + ":");
-        kLevel(root, 0, k);
-        System.out.println();
+    //   int k =2;
+    //     System.out.println("Nodes at level " + k + ":");
+    //     kLevel(root, 0, k);
+    //     System.out.println();
+    int n1 = 4, n2 = 5;
+        Node lcaNode = lca(root, n1, n2);
+        if (lcaNode != null) {
+            System.out.println("LCA of " + n1 + " and " + n2 + " is: " + lcaNode.data);
+        } else {
+            System.out.println("LCA not found.");
+        }
     }
 }
