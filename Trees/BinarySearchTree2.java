0962784c81a86f991b4fd37a926a7d6b7cf476a7
Yashwanth V <vyashwanth179@gmail.com>
2025-07-30 23:51:24 +0530

    BST to Balanced BST

--- Trees/BinarySearchTree2.java
+++ Trees/BinarySearchTree2.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 public class BinarySearchTree2 {
      public static class Node{
         int data;
@@ -42,9 +45,45 @@ public class BinarySearchTree2 {
         return root;
 
     }
+
+    //convert BST to Balanced BST
+    public static void inorder(Node root, ArrayList<Integer> inorder){
+        if(root==null) return;
+        inorder(root.left,inorder);
+        inorder.add(root.data);
+        inorder(root.right,inorder);
+    }
+    public static Node createBST1(ArrayList<Integer> inorder,int st, int end){
+        if(st>end){
+            return null;
+        }
+        int mid=(st+end)/2;
+        Node root= new Node(inorder.get(mid));
+        root.left=createBST1(inorder, st, mid-1);
+        root.right=createBST1(inorder,mid+1,end);
+        return root;
+    }
+    public static Node convertToBalancedBST(Node root) {
+       //inorder sequence
+       ArrayList<Integer> inorder= new ArrayList<>();
+       inorder(root, inorder);
+       //create balanced BST from inorder sequence
+       return createBST1(inorder, 0, inorder.size()-1);
+
+    }
     public static void main(String[] args) {
-        int[] nums={3,5,6,8,10,11,12};
-        Node root = creatBST(nums, 0, nums.length-1);
+        
+        Node root = new Node(8);
+
+        root.left= new Node(6);
+        root.left.left=new Node(5);
+        root.left.left.left=new Node(3);
+
+        root.right= new Node(10);
+        root.right.right=new Node(11);
+        root.right.right.right=new Node(12);
+
+        root= convertToBalancedBST(root);
         preorder(root);
 
     }
