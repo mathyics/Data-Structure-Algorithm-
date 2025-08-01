c335aaaee31a3838df8b96c463cab8c0240ff5f5
Yashwanth V <vyashwanth179@gmail.com>
2025-08-01 09:00:56 +0530

    Size of Largest BST in BT

--- Trees/BinarySearchTree2.java
+++ Trees/BinarySearchTree2.java
@@ -71,21 +71,62 @@ public class BinarySearchTree2 {
        return createBST1(inorder, 0, inorder.size()-1);
 
     }
+    //Size of Largest BST in a Binary Tree
+
+    static class Info{
+        int size;
+        int min;
+        int max;
+        boolean isBST;
+
+        Info(boolean isBST,int size, int min, int max) {
+            this.size = size;
+            this.min = min;
+            this.max = max;
+            this.isBST = isBST;
+        }
+    } 
+    public static int maxBST=0;
+    public static Info largestBST(Node root){
+        if(root==null){
+            return new Info(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+        Info left=largestBST(root.left);
+        Info right=largestBST(root.right);
+        int size=left.size+right.size+1;
+        int min = Math.min(root.data,Math.min(left.min, right.min));
+        int max = Math.max(root.data,Math.max(left.max, right.max));
+
+        //check its valid bst or not
+        if(root.data<=left.max||root.data>=right.min){
+            return new Info(false, size, min,max);
+
+        }
+        if(left.isBST&&right.isBST){
+            maxBST=Math.max(size, maxBST);
+            return new Info(true, size, min, max);
+        }
+
+        return new Info (false, size ,min, max);
+    }
+
     public static void main(String[] args) {
         
-        Node root = new Node(8);
-
-        root.left= new Node(6);
+        Node root = new Node(50);
+        root.left=new Node(30);
         root.left.left=new Node(5);
-        root.left.left.left=new Node(3);
+        root.left.right=new Node(20);
 
-        root.right= new Node(10);
-        root.right.right=new Node(11);
-        root.right.right.right=new Node(12);
+        root.right= new Node(60);
+        root.right.left= new Node(45);
+        root.right.right= new Node(70);
+        root.right.right.left= new Node(65);
+        root.right.right.right= new Node(80);
 
-        root= convertToBalancedBST(root);
-        preorder(root);
-
+    Info info = largestBST(root);
+        System.out.println("Size of Largest BST in the Binary Tree: " + maxBST);
+        
+      
     }
     
 
