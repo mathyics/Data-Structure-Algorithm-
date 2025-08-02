e706e129b251b1a996c648dd56e7e8cc2f0451aa
Yashwanth V <vyashwanth179@gmail.com>
2025-08-02 13:58:18 +0530

    Activity selection without sorting

--- Greedy Algorithm/Greedy.java
+++ Greedy Algorithm/Greedy.java
@@ -2,23 +2,32 @@ import java.util.*;
 import javax.xml.stream.events.EndDocument;
 class Greedy{
     public static void main(String[] args) {
-        int start[] = {1,3, 0, 5, 8, 5};
-        int finish[] = {2, 4, 6, 7, 9, 9};
+        int start[] = {0,1,3,5,5,8};
+        int finish[] = {6,2,4,7,9,9};
+        
+        int[][] activities= new int[start.length][3];
 
+        for(int i=0;i<start.length;i++){
+            activities[i][0]=i;
+            activities[i][1]=start[i];
+            activities[i][2]=finish[i]; 
+        }
+// sort using lamda expression
+         Arrays.sort(activities,Comparator.comparingDouble(o->o[2]));
         //end time basis sorted
         ArrayList<Integer> ans= new ArrayList<>();
         int maxActivity = 0;
 
         //1st activity
         maxActivity=1;
-        ans.add(0);
-        int lastEnd=finish[0];
+        ans.add(activities[0][0]);
+        int lastEnd=activities[0][2];
 
         for(int i=1;i<finish.length;i++){
-            if(start[i]>=lastEnd){
+            if(activities[i][1]>=lastEnd){
                 maxActivity++;
-                ans.add(i);
-                lastEnd=finish[i];
+                ans.add(activities[i][0]);
+                lastEnd=activities[i][2];
             }
         }
         System.out.println("Maximum number of activities: " + maxActivity);
