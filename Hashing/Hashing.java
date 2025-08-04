d09794f0103c3199c69a7934d7b779ef2c5ba49d
Yashwanth V <vyashwanth179@gmail.com>
2025-08-04 23:23:44 +0530

    HashMap basic operation

---
+++ Hashing/Hashing.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Hashing {
+    public static void main(String[] args) {
+        //Create HashMap
+        HashMap<String,Integer> map= new HashMap<>();
+        //insert
+        map.put("India",100);
+        map.put("USA",50);
+        map.put("China",150);
+
+        System.out.println("HashMap: " + map);
+
+        //Get value for a key
+        System.out.println(map.get("India"));
+
+        
+        //Check if key exists cotainsKey
+        if(map.containsKey("USA")){
+            System.out.println("USA exists in the map with value: " + map.get("USA"));
+        } else {
+            System.out.println("USA does not exist in the map.");
+        }
+
+        // Remove a key-value pair
+        map.remove("China");
+        System.out.println("After removing China: " + map);
+        // size of the map
+        System.out.println("Size of the map: " + map.size());
+        // clear th map 
+        map.clear();
+        // Check if the map is empty
+        if(map.isEmpty()){
+            System.out.println("The map is empty.");
+        } else {
+            System.out.println("The map is not empty.");
+        }
+    }
+    
+}
