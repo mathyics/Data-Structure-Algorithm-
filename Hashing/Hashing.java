40b966c3adc1a4df2feac05ef07b58a850f9ac15
Yashwanth V <vyashwanth179@gmail.com>
2025-08-05 08:31:50 +0530

    Majority Element.

--- Hashing/Hashing.java
+++ Hashing/Hashing.java
@@ -39,19 +39,39 @@ public class Hashing {
 
 
         // Looping through the map
-        TreeMap<String, Integer> map = new TreeMap<>(); 
-        map.put("India", 100);
+        // TreeMap<String, Integer> map = new TreeMap<>(); 
+        // map.put("India", 100);
 
-        map.put("USA", 50);
-        map.put("China", 150);
-        map.put("Japan", 80);
-        map.put("Germany", 120);
+        // map.put("USA", 50);
+        // map.put("China", 150);
+        // map.put("Japan", 80);
+        // map.put("Germany", 120);
 
-        Set<String> keys=map.keySet();
-        System.out.println("map: " + map);
-        System.out.println("Keys in the map:"); 
-        for(String k: keys){
-            System.out.println("key:"+k+"  value:"+map.get(k));
+        // Set<String> keys=map.keySet();
+        // System.out.println("map: " + map);
+        // System.out.println("Keys in the map:"); 
+        // for(String k: keys){
+        //     System.out.println("key:"+k+"  value:"+map.get(k));
+        // }
+
+        // Majority Element
+        int nums[]={1,3,2,5,1,3,1,5,1};
+        HashMap<Integer, Integer> Map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(Map.containsKey(nums[i])){
+                Map.put(nums[i],Map.get(nums[i])+1);
+            }else{
+                Map.put(nums[i], 1);
+            }
+
+             //Map.put(nums[i], Map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        Set<Integer> keys=Map.keySet();
+        for(Integer key:keys){
+            if(Map.get(key)>nums.length/3){
+                System.out.println("Majority Element: " + key + " with count: " + Map.get(key));
+            }
         }
     }
     
