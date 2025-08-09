1708310c959aef4791f8121cddee2587278a0290
Yashwanth V <vyashwanth179@gmail.com>
2025-08-09 23:13:15 +0530

    Detect cycle in Directed Graph

--- Graphs/Graph2.java
+++ Graphs/Graph2.java
@@ -121,7 +121,34 @@ public class Graph2 {
             }
         }
         return true;
+
     }
+    public static boolean isCycle(ArrayList<Edge>[] graph){
+        boolean vis[]=new boolean[graph.length];
+        boolean stack[]=new boolean[graph.length];
+
+        for(int i=0;i<graph.length;i++){
+            if(!vis[i]){
+                if(isCycleUtil(graph,i,vis,stack)) return true;
+        }
+        
+    }
+    return false;
+}
+     public static boolean isCycleUtil(ArrayList<Edge>[] graph, int curr, boolean vis[], boolean stack[]){
+        vis[curr]=true;
+        stack[curr]=true;
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e=graph[curr].get(i);
+            if(stack[e.dest])return true;
+
+            if(!vis[e.dest]&&isCycleUtil(graph, e.dest, vis, stack)) 
+                return true;
+        }
+
+        stack[curr]=false;
+        return false;
+     }
     public static void main(String[] args) {
 
         //if graph doesnt has cycle its bipartite graph;
@@ -131,28 +158,13 @@ public class Graph2 {
             graph[i] = new ArrayList<>();
         }
         // 0 vertex
-        graph[0].add(new Edge(0, 1, 1));
         graph[0].add(new Edge(0, 2, 1));
-       
-
-        // 1 vertex
-        graph[1].add(new Edge(1, 0, 1));
+        graph[0].add(new Edge(0, 1, 1));
         graph[1].add(new Edge(1, 3, 1));
-        // 2 vertex
-       
-        graph[2].add(new Edge(2, 0, 1));
-        graph[2].add(new Edge(2, 4, 1));
-        // 3 vertex
-        graph[3].add(new Edge(3, 1, 1));
-        graph[3].add(new Edge(3, 4, 1));
-       
-        // 4 vertex
-
-        graph[4].add(new Edge(4, 2, 1));
-       graph[4].add(new Edge(4, 3, 1));
+        graph[2].add(new Edge(2, 03, 1));
         
 
-        System.out.println(isBipartite(graph));
+        System.out.println(isCycle(graph));
     }
 
 }
