35bbdd77c34d7f84206e46cbb51f3dd11b954714
Yashwanth V <vyashwanth179@gmail.com>
2025-08-09 16:35:29 +0530

    Fixed small error

--- Graphs/Graph2.java
+++ Graphs/Graph2.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.Array;
 import java.util.*;
+import java.util.function.BiPredicate;
 
 public class Graph2 {
     static class Edge {
@@ -91,6 +92,36 @@ public class Graph2 {
         return false;
 
     }
+
+    public static boolean isBipartite(ArrayList<Edge>[] graph){
+        int col[] = new int[graph.length];
+        Arrays.fill(col,-1);
+
+        Queue<Integer> q= new LinkedList<>();
+
+        for(int i=0;i<graph.length;i++){
+            if(col[i]==-1){
+                q.add(i);
+                col[i]=0;
+                while(!q.isEmpty()){
+                    int curr=q.remove();
+                    for(int j=0;j<graph[curr].size();j++){
+                        Edge e= graph[curr].get(j);
+                        
+                        if(col[e.dest]==-1) {
+                        int nextCol= col[curr]==0?1:0;
+                        col[e.dest]=nextCol;
+                        q.add(e.dest);
+                        }else if(col[curr]==col[e.dest]){
+                            return false;
+                        }
+
+                    }
+                }
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         int V = 7;
         ArrayList<Edge>[] graph = new ArrayList[V];
@@ -99,25 +130,27 @@ public class Graph2 {
         }
         // 0 vertex
         graph[0].add(new Edge(0, 1, 1));
-       // graph[0].add(new Edge(0, 2, 1));
-        graph[0].add(new Edge(0, 3, 1));
+        graph[0].add(new Edge(0, 2, 1));
+       
 
         // 1 vertex
         graph[1].add(new Edge(1, 0, 1));
-        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 1));
         // 2 vertex
-        //graph[2].add(new Edge(2, 0, 1));
-        graph[2].add(new Edge(2, 1, 1));
+       
+        graph[2].add(new Edge(2, 0, 1));
         graph[2].add(new Edge(2, 4, 1));
         // 3 vertex
-        graph[3].add(new Edge(3, 0, 1));
+        graph[3].add(new Edge(3, 1, 1));
+        //graph[3].add(new Edge(3, 4, 1));
        
         // 4 vertex
 
         graph[4].add(new Edge(4, 2, 1));
+       // graph[4].add(new Edge(4, 3, 1));
         
 
-        System.out.println(detectCycle(graph));
+        System.out.println(isBipartite(graph));
     }
 
 }
