cfc0f9ff1db5be12f3dc1a8997c8482acb21a987
Yashwanth V <vyashwanth179@gmail.com>
2025-08-10 17:26:08 +0530

    Topological Sort using DFS and BFS

--- Graphs/Graph2.java
+++ Graphs/Graph2.java
@@ -149,22 +149,89 @@ public class Graph2 {
         stack[curr]=false;
         return false;
      }
+
+     public static void topologicalSort(ArrayList<Edge>[] graph){
+        boolean[] vis= new boolean[graph.length];
+        Stack<Integer> s= new Stack<>();
+
+        for(int i=0;i<graph.length;i++){
+            if(!vis[i]){
+                topologicalSortUtil(graph,i,vis,s);
+            }
+        }
+        while(!s.isEmpty()){    
+            System.out.print(s.pop()+" ");
+            }
+
+
+     }
+
+      public static void topologicalSortUtil(ArrayList<Edge>[] graph,int curr, boolean[] vis, Stack<Integer> s ){
+        vis[curr]=true;
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e= graph[curr].get(i);
+            if(!vis[e.dest]){
+                topologicalSortUtil(graph, e.dest, vis, s);
+            }
+        }
+        s.push(curr);
+      }
+       
+      public static void calcIndeg(ArrayList<Edge>[] graph,int inDeg[]){
+        for(int i=0;i<graph.length;i++){
+             int v=i;
+             for(int j=0;j<graph[v].size();j++){
+                Edge e=graph[v].get(j);
+                inDeg[e.dest]++;
+             }
+        }
+      }
+
+      public static void topoBFS(ArrayList<Edge>[] graph){
+        int inDeg[]= new int[graph.length];
+        calcIndeg(graph,inDeg);
+        Queue<Integer> q= new LinkedList<>();
+        for(int i=0;i<inDeg.length;i++){
+            if(inDeg[i]==0){
+                q.add(i);
+            }
+        }
+
+        //bfs
+        while(!q.isEmpty()){
+            int curr=q.remove();
+            System.out.print(curr+" ");
+            for(int i=0;i<graph[curr].size();i++){
+                Edge e=graph[curr].get(i);
+                inDeg[e.dest]--;
+                if(inDeg[e.dest]==0){
+                    q.add(e.dest);
+                }
+            }
+        }
+      }
+
+      
+
+
     public static void main(String[] args) {
 
         //if graph doesnt has cycle its bipartite graph;
-        int V = 7;
+        int V = 6;
         ArrayList<Edge>[] graph = new ArrayList[V];
         for (int i = 0; i < V; i++) {
             graph[i] = new ArrayList<>();
         }
         // 0 vertex
-        graph[0].add(new Edge(0, 2, 1));
-        graph[0].add(new Edge(0, 1, 1));
-        graph[1].add(new Edge(1, 3, 1));
-        graph[2].add(new Edge(2, 03, 1));
+        graph[2].add(new Edge(2, 3, 1));
+        graph[3].add(new Edge(3, 1, 1));
+        graph[4].add(new Edge(4, 0, 1));
+        graph[4].add(new Edge(4, 1, 1));
+        graph[5].add(new Edge(5, 0, 1));
+        graph[5].add(new Edge(5, 2, 1));
         
 
-        System.out.println(isCycle(graph));
+       // printAllPath(graph, 5, 1, "");
     }
 
 }
