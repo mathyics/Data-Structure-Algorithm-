c15ee3200bc23f264bef63e2cca1d2f7a6ddfcce
Yashwanth V <vyashwanth179@gmail.com>
2025-08-11 09:06:56 +0530

    Prims MST

--- Graphs/Graph2.java
+++ Graphs/Graph2.java
@@ -238,7 +238,7 @@ public class Graph2 {
         public int compareTo(Pair p2) {
             return this.path - p2.path;
         }
-        
+
     }
 
     public static void dijkstra(ArrayList<Edge>[] graph, int src) {
@@ -272,26 +272,97 @@ public class Graph2 {
             System.out.print(dist[i]+" ");
         }
     }
+    public static void bellmanFold(ArrayList<Edge>[] graph,int src ){
+        int dist[]= new int[graph.length];
 
+        for(int i=0;i<graph.length;i++){
+            if(src!=i){
+                dist[i]=Integer.MAX_VALUE;
+            }
+        }
+        int V=graph.length;
+        for(int i=0;i<V-1;i++){
+
+            for(int j=0;j<graph.length;j++){
+                for(int k=0;k<graph[j].size();k++){
+                    Edge e=graph[j].get(k);
+                    int u=e.src;
+                    int v=e.dest;
+                    int wt=e.wt;
+                    if((dist[u]!=Integer.MAX_VALUE)&&(dist[u]+wt)<dist[v]){
+                        dist[v]=dist[u]+wt;
+                    }
+                }
+            }
+        }
+
+        for(int i=0;i<dist.length;i++){
+            System.out.print(dist[i]+" ");
+        }
+
+    }
+
+    static  class Pair2 implements Comparable<Pair2>{
+        int v;
+        int cost;
+        public Pair2(int v,int cost){
+            this.v=v;
+            this.cost=cost;
+        }
+
+        @Override
+        public int compareTo(Pair2 p2){
+            return this.cost-p2.cost;
+        }
+    }
+
+    public static void prims(ArrayList<Edge>[] graph){
+        boolean vis[]=new boolean[graph.length];
+        PriorityQueue<Pair2>pq=new PriorityQueue<>();
+        pq.add(new Pair2(0,0));
+        int finalcost=0;
+
+        while(!pq.isEmpty()){
+            Pair2 curr=pq.remove();
+            if(!vis[curr.v]){
+                vis[curr.v]=true;
+                finalcost+=curr.cost;
+
+                for(int i=0;i<graph[curr.v].size();i++){
+                    Edge e=graph[curr.v].get(i);
+                    pq.add(new Pair2(e.dest, e.wt));
+                }
+            }
+        }
+
+        System.out.println("Minimum cost of MST "+finalcost) ;
+
+
+    }
     public static void main(String[] args) {
 
         // if graph doesnt has cycle its bipartite graph;
-        int V = 6;
+        int V = 5;
         ArrayList<Edge>[] graph = new ArrayList[V];
         for (int i = 0; i < V; i++) {
             graph[i] = new ArrayList<>();
         }
         // 0 vertex
-        graph[0].add(new Edge(0, 1, 2));
-        graph[0].add(new Edge(0, 2, 4));
-        graph[1].add(new Edge(1, 3, 7));
-        graph[1].add(new Edge(1, 2, 1));
-        graph[2].add(new Edge(2, 4, 3));
-        graph[3].add(new Edge(3, 5, 1));
-        graph[4].add(new Edge(4, 3, 2));
-        graph[4].add(new Edge(4, 5, 5));
+        graph[0].add(new Edge(0, 1, 10));
+        graph[0].add(new Edge(0, 2, 15));
+        graph[0].add(new Edge(0, 3, 30));
 
-       dijkstra(graph, 0);
+        graph[1].add(new Edge(1, 0, 10));
+        graph[1].add(new Edge(0, 3, 40));
+      
+        graph[2].add(new Edge(2, 0, 15));
+        graph[2].add(new Edge(2, 3, 50));
+
+        graph[3].add(new Edge(3, 1, 40));
+        graph[3].add(new Edge(4, 2, 50));
+       
+
+    prims(graph);
     }
 
 }
