7a6fde9e58ddef39ee818e4757b743a458778aef
Yashwanth V <vyashwanth179@gmail.com>
2025-08-12 22:42:34 +0530

    COnnecting n cities

--- Graphs/Graph5.java
+++ Graphs/Graph5.java
@@ -1,86 +1,140 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.*;
 
 public class Graph5 {
-    
-   static class Edge{
-    int src;
-    int dest;
-    int wt;
 
-    public Edge(int src,int dest, int wt){
-        this.src=src;
-        this.dest=dest;
-        this.wt=wt;
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
     }
-    }
-    public static void creatGraph(int[][] flight,ArrayList<Edge>[] graph){
-        for(int i=0;i<graph.length;i++){
-            graph[i]=new ArrayList<>();
+
+    static class Edge2 implements Comparable<Edge2> {
+        int dest;
+        int cost;
+
+        public Edge2(int dest, int cost) {
+            this.dest = dest;
+            this.cost = cost;
         }
 
-        for(int i=0;i<flight.length;i++){
-            int src=flight[i][0];
-            int dest=flight[i][1];
-            int wt= flight[i][2];
-            Edge e= new Edge(src, dest, wt);
+        @Override
+        public int compareTo(Edge2 e) {
+            return this.cost - e.cost;
+        }
+    }
+
+    public static void creatGraph(int[][] flight, ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < flight.length; i++) {
+            int src = flight[i][0];
+            int dest = flight[i][1];
+            int wt = flight[i][2];
+            Edge e = new Edge(src, dest, wt);
             graph[src].add(e);
         }
 
     }
-    static class Info{
+
+    static class Info {
         int v;
         int cost;
         int stops;
-        public Info(int v, int cost, int stops){
-            this.v=v;
-            this.cost=cost;
-            this.stops=stops;
+
+        public Info(int v, int cost, int stops) {
+            this.v = v;
+            this.cost = cost;
+            this.stops = stops;
         }
     }
-    public static int cheastFlight(int n,int src, int dest,int k, ArrayList<Edge> graph[]){
+
+    public static int cheastFlight(int n, int src, int dest, int k, ArrayList<Edge> graph[]) {
         int dist[] = new int[n];
 
-        for(int i=0;i<n;i++){
-            if(src!=i){
-                dist[i]=Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (src != i) {
+                dist[i] = Integer.MAX_VALUE;
             }
         }
-        Queue<Info> q= new LinkedList<>();
+        Queue<Info> q = new LinkedList<>();
         q.add(new Info(src, 0, 0));
 
-        while(!q.isEmpty()){
-            Info curr=q.remove();
-            if(curr.stops>k){
+        while (!q.isEmpty()) {
+            Info curr = q.remove();
+            if (curr.stops > k) {
                 break;
             }
 
-            for(int i=0;i<graph[curr.v].size();i++){
-                Edge e=graph[curr.v].get(i);
-               // int u=e.src;
-                int v=e.dest;
-                int wt=e.wt;
+            for (int i = 0; i < graph[curr.v].size(); i++) {
+                Edge e = graph[curr.v].get(i);
+                // int u=e.src;
+                int v = e.dest;
+                int wt = e.wt;
 
-                if(curr.cost+wt<dist[v]&&curr.stops<=k){
-                    dist[v]=curr.cost+wt;
-                   q.add(new Info(v, dist[v], curr.stops+1));
+                if (curr.cost + wt < dist[v] && curr.stops <= k) {
+                    dist[v] = curr.cost + wt;
+                    q.add(new Info(v, dist[v], curr.stops + 1));
                 }
             }
         }
-        if(dist[dest]==Integer.MAX_VALUE){
-            return  -1;
-        }else{
+        if (dist[dest] == Integer.MAX_VALUE) {
+            return -1;
+        } else {
             return dist[dest];
         }
     }
+
+    // connect n cities;
+    public static int connectCities(int[][] cities) {
+        PriorityQueue<Edge2> pq = new PriorityQueue<>();
+        boolean vis[] = new boolean[cities.length];
+        int finalCost = 0;
+        pq.add(new Edge2(0, 0));
+        while (!pq.isEmpty()) {
+            Edge2 curr = pq.remove();
+            if (!vis[curr.dest]) {
+                vis[curr.dest] = true;
+                finalCost += curr.cost;
+
+                for (int i = 0; i < cities[curr.dest].length; i++) {
+                    if (cities[curr.dest][i] != 0) {
+                        pq.add(new Edge2(i, cities[curr.dest][i]));
+                    }
+                }
+
+            }
+        }
+
+        return finalCost;
+    }
+
     public static void main(String[] args) {
-        int n=4;
-        int[][] flight={{0,1,100},{1,2,100},{2,0,100},{1,3,600},{2,3,300}};
-        int src=0, dest=3,k=1;
-        ArrayList<Edge>[] graph=new ArrayList[n];
-        creatGraph(flight, graph);
-       System.err.println(cheastFlight(n, src,dest, k, graph)); 
+        // int n=4;
+        // int[][] flight={{0,1,100},{1,2,100},{2,0,100},{1,3,600},{2,3,300}};
+        // int src=0, dest=3,k=1;
+        // ArrayList<Edge>[] graph=new ArrayList[n];
+        // creatGraph(flight, graph);
+        // System.err.println(cheastFlight(n, src,dest, k, graph));
+
+        int[][] cities = { 
+                { 0, 1, 2, 3, 4 },
+                { 1, 0, 5, 0, 7 },
+                { 2, 5, 0, 6, 0 },
+                { 3, 0, 6, 0, 0 },
+                { 4, 7, 0, 0, 0 } };
+
+                System.out.println(connectCities(cities));
 
     }
 }
