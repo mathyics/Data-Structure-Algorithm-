346510f4e494fa321e770edc4f54ff2ea476cae3
Yashwanth V <vyashwanth179@gmail.com>
2025-08-13 09:19:16 +0530

    Fibaonacci in Tabulation

--- Dp/Dp1.java
+++ Dp/Dp1.java
@@ -20,10 +20,24 @@ public class Dp1 {
 
         return fib[n];
     }
+
+    // fibonacci in tabulation 
+
+    public static int fibTab(int n){// time over 3600
+        int dp[]=new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+
+        for(int i=2;i<n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+
+    return dp[n];
+    }
     public static void main(String[] args) {
         int n=10;
         long startTime=System.nanoTime();
-        int result=fib(n,new int[n+1]);
+        int result=fibTab(n);
         long endTime=System.nanoTime();
 
         System.out.println(result);
