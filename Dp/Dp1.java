7220a15dec130fb7f946e5c82423a4633bf3b49c
Yashwanth V <vyashwanth179@gmail.com>
2025-08-13 13:19:33 +0530

    Climbing stairs Memoization

--- Dp/Dp1.java
+++ Dp/Dp1.java
@@ -23,21 +23,37 @@ public class Dp1 {
 
     // fibonacci in tabulation 
 
-    public static int fibTab(int n){// time over 3600
+    public static int fibTab(int n){// time 1900
         int dp[]=new int[n+1];
         dp[0]=0;
         dp[1]=1;
 
-        for(int i=2;i<n;i++){
+        for(int i=2;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
 
     return dp[n];
     }
+    public static int climbStairsMem(int n, int[] dp){
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            return 0;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n]=climbStairsMem(n-1, dp)+climbStairsMem(n-2, dp);
+        return dp[n];
+
+    }
     public static void main(String[] args) {
         int n=10;
+        int[] dp= new int[n+1];
+        Arrays.fill(dp,-1);
         long startTime=System.nanoTime();
-        int result=fibTab(n);
+        int result=climbStairsMem(n, dp);
         long endTime=System.nanoTime();
 
         System.out.println(result);
