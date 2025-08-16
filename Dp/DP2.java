9b91c48b0882b6a744712cc3ea413d3fd4d87601
Yashwanth V <vyashwanth179@gmail.com>
2025-08-16 16:18:17 +0530

    knapack using tabulation

--- Dp/DP2.java
+++ Dp/DP2.java
@@ -37,6 +37,41 @@ public static int knapsackMem(int val[], int wt[],int W, int n,int[][] dp){
 
 }
 
+public static int knapsackTab(int val[], int wt[], int W ){
+    int n=val.length;
+    int[][] dp=new int[n+1][W+1];
+    // initialize first row and first colum as 0
+    for(int i=0;i<dp.length;i++){
+        dp[i][0]=0;
+    }
+    for(int i=1;i<dp[0].length;i++){
+        dp[0][i]=0;
+    }
+
+    for(int i=1;i<dp.length;i++){
+        for(int j=1;j<dp[0].length;j++){
+            int v=val[i-1];
+            int w=wt[i-1];
+            if(w<=j){
+                int include=v+dp[i-1][j-w];
+                int exclude=dp[i-1][j];
+                dp[i][j]=Math.max(include, exclude);
+            }else{
+                int exclude=dp[i-1][j];
+                dp[i][j]=exclude;
+            }
+        }
+    }
+     for(int i=1;i<dp.length;i++){
+        for(int j=1;j<dp[0].length;j++){
+            System.out.print(dp[i][j]+" ");
+        }
+        System.out.println();
+    }
+
+    return dp[n][W];
+}
+
     public static void main(String[] args) {
      int val[]={15,14,10,45,30};
      int wt[]={2,5,1,3,4};
@@ -50,13 +85,9 @@ public static int knapsackMem(int val[], int wt[],int W, int n,int[][] dp){
         }
     }
     long start=System.nanoTime();
-    System.out.println(knapsack(val,wt,W,n));
+    System.out.println(knapsackTab(val, wt, W));
     long end= System.nanoTime();
     System.out.println(end-start);
-  long  s=System.nanoTime();
-    System.out.println(knapsackMem(val,wt,W,n,dp));
-   long e= System.nanoTime();
-
-    System.out.println(e-s);
+ 
  }   
 }
