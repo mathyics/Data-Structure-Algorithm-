76371b8b2b9e957a374abd5ef0358b9c48108a55
Yashwanth V <vyashwanth179@gmail.com>
2025-08-16 08:46:07 +0530

    KnapSack With Memoization

--- Dp/DP2.java
+++ Dp/DP2.java
@@ -15,11 +15,48 @@ public static int knapsack(int val[], int wt[],int W, int n){
 
 }
 
+public static int knapsackMem(int val[], int wt[],int W, int n,int[][] dp){
+    if(W==0||n==0){
+        return 0;
+    }
+    if(dp[n][W]!=-1){
+        return dp[n][W];
+    }
+    if(wt[n-1]<=W){
+        int ans1=val[n-1]+knapsackMem(val, wt, W-wt[n-1], n-1,dp);
+        int ans2=knapsackMem(val, wt, W, n-1,dp);
+        dp[n][W]=Math.max(ans1, ans2);
+
+       return dp[n][W];
+    }else{
+         dp[n][W]=knapsackMem(val, wt, W, n-1,dp);
+
+     return dp[n][W];
+    }
+    
+
+}
+
     public static void main(String[] args) {
      int val[]={15,14,10,45,30};
      int wt[]={2,5,1,3,4};
      int W=7;
      int n=val.length;
-     System.out.println(knapsack(val, wt, W, n));
+    int[][] dp = new int[n+1][W+1];
+
+    for (int i=0;i<dp.length;i++){
+        for(int j=0;j<dp[0].length;j++){
+            dp[i][j]=-1 ;
+        }
+    }
+    long start=System.nanoTime();
+    System.out.println(knapsack(val,wt,W,n));
+    long end= System.nanoTime();
+    System.out.println(end-start);
+  long  s=System.nanoTime();
+    System.out.println(knapsackMem(val,wt,W,n,dp));
+   long e= System.nanoTime();
+
+    System.out.println(e-s);
  }   
 }
