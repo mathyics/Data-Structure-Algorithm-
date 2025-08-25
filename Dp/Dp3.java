7f405c7be3bc2b840559ce3723d6386f04fdc184
Yashwanth V <vyashwanth179@gmail.com>
2025-08-26 00:20:13 +0530

    LCS using memoization

--- Dp/Dp3.java
+++ Dp/Dp3.java
@@ -48,6 +48,24 @@ public class Dp3 {
             return Math.max(ans1, ans2);
         }
     }
+
+     public static int lcs2(String s1,String s2, int n,int m,int dp[][]){
+        if(n==0 || m==0){
+            return 0;
+        }
+
+        if(dp[n][m]!=-1){
+            return dp[n][m];
+        }
+        if(s1.charAt(n-1)==s2.charAt(m-1)){
+            return dp[n][m]=lcs(s1,s2,n-1,m-1)+1;
+        }else{
+            int ans1=lcs(s1,s2,n-1,m);
+            int ans2=lcs(s1, s2, n, m-1);
+            return dp[n][m]=Math.max(ans1, ans2);
+        }
+    }
+    
     public static void main(String[] args) {
         // int[] coins = {1, 2, 5};
         // int amount = 5;
@@ -63,6 +81,13 @@ public class Dp3 {
         String s2="ace";
         int n=s1.length();
         int m=s2.length();
-        System.out.println("LCS length: " + lcs(s1, s2, n, m));
+
+        int[][] dp=new int[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                dp[i][j]=-1;
+            }
+        }
+        System.out.println("LCS length: " + lcs2(s1, s2, n, m,dp));
     }
 }
