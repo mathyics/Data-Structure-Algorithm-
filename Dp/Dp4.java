a44cdaae9d5ce4b632aa07db44781bcde3b24519
Yashwanth V <vyashwanth179@gmail.com>
2025-08-27 23:23:41 +0530

    Longest increasing Subsequence

--- Dp/Dp4.java
+++ Dp/Dp4.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Dp4 {
 
     public static int LCSub(String s1,String s2 ){
@@ -20,6 +24,44 @@ public class Dp4 {
         }
 
         return ans;
+    }
+
+     public int lengthOfLIS(int[] nums) {
+        Set<Integer> set= new HashSet<>();
+       
+       for(int i=0;i<nums.length;i++){
+        set.add(nums[i]);
+       }
+
+       int nums2[]=new int[set.size()];
+
+       int m=0;
+       for(int key:set){
+        nums2[m]=key;
+        m++;
+       }
+
+       Arrays.sort(nums2);
+
+        int n=nums.length;
+       int k=nums2.length;
+        int dp[][]=new int[n+1][m+1];
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<k+1;j++){
+                if(nums[i-1]==nums2[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else{
+                    int ans1=dp[i-1][j];
+                    int ans2=dp[i][j-1];
+                    dp[i][j]=Math.max(ans1,ans2);
+                }
+            }
+        }
+
+
+        return dp[n][k];
+        
     }
     public static void main(String[] args) {
         String s1="ABCDE";
