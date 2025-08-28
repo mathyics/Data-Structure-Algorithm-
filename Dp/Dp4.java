550753b5d59f4a46a52c5fd1a8c0d11679ee6e19
Yashwanth V <vyashwanth179@gmail.com>
2025-08-28 23:42:18 +0530

    Edit DIstance

--- Dp/Dp4.java
+++ Dp/Dp4.java
@@ -63,10 +63,40 @@ public class Dp4 {
         return dp[n][k];
         
     }
+
+
+    public static int editDist(String s1,String s2){
+        int n= s1.length();
+        int m=s2.length();
+        int dp[][]=new int[n+1][m+1];
+
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<m+1;j++){
+            dp[0][j]=j;
+        }
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    int insert= dp[i][j-1];
+                    int delete= dp[i-1][j];
+                    int replace= dp[i-1][j-1];
+
+                    dp[i][j]= 1+ Math.min(insert, Math.min(delete, replace));
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
     public static void main(String[] args) {
-        String s1="ABCDE";
-        String s2="ABGCE";
-        System.out.println(LCSub(s1,s2));
+        String s1="include";
+        String s2="exclude";
+        System.out.println(editDist(s1, s2));
     }
     
 }
