3c021e7b0f99935f7dac3e75d37997d96b8494b9
Yashwanth V <vyashwanth179@gmail.com>
2025-08-29 23:32:26 +0530

    Catalon Number

--- Dp/Dp5.java
+++ Dp/Dp5.java
@@ -33,11 +33,55 @@ public class Dp5{
 
         return dp[n][m];
       }
+
+      public static int catalanRec(int n){
+        if(n==0||n==1) return 1;
+
+        int ans=0;
+
+        for(int i=0;i<n;i++){
+            ans+=catalanRec(i)*catalanRec(n-i-1);
+        }
+
+        return ans;
+      }
+
+      public static int catalanMem(int n, int dp[]){
+        if(n==0||n==1) return 1;
+
+        if(dp[n]!=0) return dp[n];
+
+        int ans=0;
+
+        for(int i=0;i<n;i++){
+            ans+=catalanMem(i,dp)*catalanMem(n-i-1,dp);
+
+        }
+
+        return dp[n]=ans;
+      }
+        public static int catalanTab(int n){
+            int dp[]=new int[n+1];
+            dp[0]=1;
+            dp[1]=1;
+
+            for(int i=2;i<n+1;i++){
+                for(int j=0;j<i;j++){
+                    dp[i]+=dp[j]*dp[i-j-1];
+                }
+            }
+
+            return dp[n];
+
+        }
     public static void main(String args[]){
 
-        String s="adceb";
-        String p="*a*b";
+        // String s="adceb";
+        // String p="*a*b";
 
-        System.out.println(isMatch(s,p));
+        // System.out.println(isMatch(s,p));
+
+        int n=4;
+        System.out.println(catalanTab(n));
     }
 }
