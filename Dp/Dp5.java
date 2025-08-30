63f92383a4ba3fbf3e753f409f3d43bb7bfbbe89
Yashwanth V <vyashwanth179@gmail.com>
2025-08-30 08:42:09 +0530

    Count BST

--- Dp/Dp5.java
+++ Dp/Dp5.java
@@ -74,6 +74,20 @@ public class Dp5{
             return dp[n];
 
         }
+
+        public static int countBST(int n){
+            int dp[]=new int[n+1];
+            dp[0]=1;
+            dp[1]=1;
+
+            for(int i=2;i<n+1;i++){
+                for(int j=0;j<i;j++){
+                    dp[i]+=dp[j]*dp[i-j-1];
+                }
+            }
+
+            return dp[n];
+        }
     public static void main(String args[]){
 
         // String s="adceb";
@@ -82,6 +96,6 @@ public class Dp5{
         // System.out.println(isMatch(s,p));
 
         int n=4;
-        System.out.println(catalanTab(n));
+        System.out.println(countBST(n));
     }
 }
